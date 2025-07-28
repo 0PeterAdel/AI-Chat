7e942263f4a2efe69777398ab6f291bfd43c3f0b
0PeterAdel <1peteradel@gmail.com>
2025-07-28 17:15:34 +0300

    Remove FrontEnd To best Start

--- frontend/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css' // <-- تأكد من وجود هذا السطر لاستيراد ملف CSS
-
-createRoot(document.getElementById("root")!).render(<App />);
